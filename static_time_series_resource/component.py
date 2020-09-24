95f336bd8fee2c82463b0c19adf8208d9700e262
Otto Hylli <otto.hylli@tuni.fi>
2020-09-24 18:25:29 +0300

    Add basic general simulation component features to STaticTimeSeriesResource
    Now just logs each epoch adnd does nothing else.

--- static_time_series_resource/component.py
+++ static_time_series_resource/component.py
@@ -3,20 +3,36 @@ Created on 23.9.2020
 
 @author: hylli
 '''
+import asyncio
 
 from tools.components import AbstractSimulationComponent
+from tools.tools import FullLogger
+
+LOGGER = FullLogger( __name__ )
 
 class StaticTimeSeriesResource(AbstractSimulationComponent):
     '''
     classdocs
     '''
 
-
-    def __init__(self, params):
+    def __init__(self):
         '''
         Constructor
         '''
-        pass
+        super().__init__()
+        
+    async def start_epoch(self) -> bool:
+        if not await super().start_epoch():
+            return False
+        
+        LOGGER.info( 'Starting epoch.' )
+        return True
+
+async def start_component():
+    resource = StaticTimeSeriesResource()
+    await resource.start()
+    while not resource.is_stopped:
+        await asyncio.sleep( 2 )
         
 if __name__ == '__main__':
-    print( 'test' )
+    asyncio.run(start_component())
