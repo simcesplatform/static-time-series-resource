e6475348acf6ff746365cd8cb136591fe1ad0e91
Otto Hylli <otto.hylli@tuni.fi>
2020-09-24 10:47:33 +0300

    Start for the project containing basic stucture and implementation stubb

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Static time series resource
+
+A simulation platform component used to simulate simple loads and generators whose published states are determined by a file containing a simple time series of attribute values for each epoch. 
---
+++ static_time_series_resource/component.py
@@ -0,0 +1,22 @@
+'''
+Created on 23.9.2020
+
+@author: hylli
+'''
+
+from tools.components import AbstractSimulationComponent
+
+class StaticTimeSeriesResource(AbstractSimulationComponent):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        pass
+        
+if __name__ == '__main__':
+    print( 'test' )
